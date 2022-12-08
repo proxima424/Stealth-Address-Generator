49d24ab3b1511151cee8c68b2d7f3602e0578328
Prince Singhal <82727098+PRINCEPOPOSOS@users.noreply.github.com>
2022-12-08 15:31:09 +0530

    Add files via upload

---
+++ src/templates/testingFile.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract FactoryAssembly {
+    event Deployed(address addr, uint salt);
+
+    function getBytecode(bytes32 _hash) public pure returns (bytes memory) {
+        bytes memory bytecode = type(TestContract).creationCode;
+
+        return abi.encodePacked(bytecode, abi.encode(_hash));
+    }
+
+    function getAddress(bytes memory bytecode, uint _salt) public view returns (address){
+        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(bytecode)));
+        return address(uint160(uint(hash)));
+    }
+
+    function deploy(bytes memory bytecode, uint _salt) public payable {
+        address addr;
+        assembly {
+            addr := create2(callvalue(),add(bytecode, 0x20),mload(bytecode), _salt )
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+
+        emit Deployed(addr, _salt);
+    }
+}
+
+contract TestContract {
+
+    bytes32 private hashOfB;
+
+    constructor(bytes32 _hash) payable{
+        hashOfB = _hash;
+    }
+
+    modifier isB{
+        require(keccak256(abi.encode(msg.sender)) == hashOfB, "you are not the right person");
+        _;
+    }
+
+    function getBalance() public view isB returns (uint) {
+        return address(this).balance;
+    }
+
+    function withdraw(uint _amount) public isB{
+            payable(address(msg.sender)).transfer(_amount * (10**18));
+    }
+
+    receive() external payable{}
+}
