ad6c4647f62acb4b0244c19b62b1ec4edd32433f
Prince Singhal <82727098+0xSyCaMoRe@users.noreply.github.com>
2022-12-11 11:35:16 +0530

    Add files via upload

--- src/templates/TransferFundsERC721.sol
+++ src/templates/TransferFundsERC721.sol
@@ -2,45 +2,47 @@
 pragma solidity ^0.8.0;
 
 import {ECDSA} from "../lib/ECDSA.sol";
-import {IERC721} from "../lib/IERC721.sol";
+// import {IERC20} from "../lib/IERC721.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract TransferFundsERC721 {
+contract TransferFundsERC721{
     using ECDSA for *;
 
     bytes32 private hashOfAddressB;
     string public messageToSign;
 
-    constructor(bytes32 _hashOfAddressB, string memory _messageToSign) {
+    constructor(bytes32 _hashOfAddressB, string memory _messageToSign) public {
         _hashOfAddressB = hashOfAddressB;
         messageToSign = _messageToSign;
     }
 
-    function contractTokenBalance(address _nftContract)
+    function contractTokenBalance(address _tokenId)
         public
         view
         returns (uint256)
     {
-        require(_tokenAddress != address(0), "INVALID_ADDRESS");
-        return IERC721(_nftContract).balanceOf(address(this));
+        require(_tokenId != address(0), "INVALID_ADDRESS");
+        return IERC721(_tokenId).balanceOf(address(this));
     }
 
     function withdraw(
-        uint256 _tokenId,
-        address _nftContract,
+        uint _tokenId,
         bytes32 _Ethhash,
-        bytes memory _signature
+        bytes memory _signature,
+        address _nftContract
     )
         public
         returns (bool success)
     {
+        require(IERC721(_nftContract).ownerOf(_tokenId)==address(this),"this contract doesn't have the token");
         require(
             keccak256(abi.encodePacked(ECDSA.recover(_Ethhash, _signature)))
                 == hashOfAddressB,
             "INVALID_SIGNATURE"
         );
-        IERC721(_nftContract).transferFrom(address(this), msg.sender, _tokenId);
+        IERC721(_nftContract).safeTransferFrom(address(this), msg.sender, _tokenId);
         return true;
     }
-
+    
     receive() external payable {}
 }
